07f36cdc8eeb0d376d4fe916c6d88860db9ed26a
groggy <144368137+Groggui@users.noreply.github.com>
2023-09-30 01:32:34 +0300

    Update hw4.py

--- homework4/hw4.py
+++ homework4/hw4.py
@@ -1,1 +1,10 @@
-
+list = open('C:/train.csv')
+count_men, count_women = 0, 0
+for line in list:
+    line = line.split(',')
+    if line[1] == '1' and line[5] == 'male':
+        count_men += 1
+    if line[1] == '1' and line[5] == 'female':
+        count_women += 1
+print('выжило мужчин:', count_men)
+print('выжило женщин:', count_women)
